c68b2be0e73b5d7173ab30e64b14ef6732991a38
Karl Mark Balagtey <karl.balagtey@gmail.com>
2019-08-13 14:58:53 +0100

    Updates

---
+++ precache-manifest.fea826d43faa0c6da3ecbcea2c1d9bbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eecf834a949bd8dec57e0277e0e038f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "88ab132bbf898a8ae6e1",
+    "url": "/monsters-rolodex/static/css/main.74621c75.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "88ab132bbf898a8ae6e1",
+    "url": "/monsters-rolodex/static/js/main.7b65d623.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
